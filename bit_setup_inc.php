0df0e682ed519107713c5b789db2123fd3ba2be7
Max Kremmel <xing@synapse.plus.com>
2006-09-01 08:10:46 +0000

    include BitBoardTopic for services

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -2,8 +2,8 @@
 global $gBitSystem;
 
 $registerHash = array(
-'package_name' => 'bitboards',
-'package_path' => dirname( __FILE__ ).'/',
+	'package_name' => 'bitboards',
+	'package_path' => dirname( __FILE__ ).'/',
 );
 $gBitSystem->registerPackage( $registerHash );
 
@@ -11,6 +11,7 @@ if( $gBitSystem->isPackageActive( 'bitboards' ) ) {
 	$gBitSystem->registerAppMenu( BITBOARDS_PKG_NAME, ucfirst( BITBOARDS_PKG_DIR ), BITBOARDS_PKG_URL.'index.php', 'bitpackage:bitboards/menu_bitboards.tpl', BITBOARDS_PKG_NAME );
 
 	require_once( BITBOARDS_PKG_PATH.'BitBoard.php' );
+	require_once( BITBOARDS_PKG_PATH.'BitBoardTopic.php' );
 
 	$gLibertySystem->registerService( LIBERTY_SERVICE_FORUMS, BITBOARDS_PKG_NAME, array(
 		'content_display_function' => 'bitboards_content_display',
