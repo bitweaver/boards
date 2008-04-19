500d5a8aa9ac53d3c6d6a698f64c9c3afc847f1c
Christian Fowler <spider@viovio.com>
2008-04-19 18:11:55 +0000

    remove switchboard dependency, migreate to BitMailer, and return Message-ID email header on sendEmail and store

--- boards_comments_inc.php
+++ boards_comments_inc.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_boards/boards_comments_inc.php,v 1.2 2008/04/16 23:53:49 spiderr Exp $
- * $Id: boards_comments_inc.php,v 1.2 2008/04/16 23:53:49 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_boards/boards_comments_inc.php,v 1.3 2008/04/19 18:11:55 spiderr Exp $
+ * $Id: boards_comments_inc.php,v 1.3 2008/04/19 18:11:55 spiderr Exp $
  *
  * intermediate include file to provide centralized place to pre/post handle comments_inc include
  *
  * @author spider <spider@steelsun.com>
- * @version $Revision: 1.2 $ $Date: 2008/04/16 23:53:49 $ $Author: spiderr $
+ * @version $Revision: 1.3 $ $Date: 2008/04/19 18:11:55 $ $Author: spiderr $
  * @package boards
  */
 
 
 require_once (LIBERTY_PKG_PATH.'comments_inc.php');
 
-if( $gBitSystem->isPackageActive( 'switchboard' ) && !empty( $storeComment ) && $gBoard->getPreference('boards_mailing_list') ) {
+if( !empty( $storeComment ) && $gBoard->getPreference('boards_mailing_list') ) {
 	if( empty( $storeComment->mErrors ) ) {
 		$storeComment->loadComment();
-		global $gSwitchboardSystem;
-		require_once( SWITCHBOARD_PKG_PATH.'SwitchboardSystem.php' );
+		require_once( KERNEL_PKG_PATH.'BitMailer.php' );
+		$bitMailer = new BitMailer();
 		$email = $gBoard->getPreference('boards_mailing_list').'@'.$gBitSystem->getConfig( 'boards_email_host', $gBitSystem->getConfig( 'kernel_server_name' ) );
 		$headerHash['mail_from'] = $gBitSystem->getConfig( 'boards_sync_user' ).'@'.$gBitSystem->getConfig( 'boards_sync_mail_server' );
 		if( $storeComment->getField( 'user_id' ) == ANONYMOUS_USER_ID ) {
@@ -27,7 +27,9 @@ if( $gBitSystem->isPackageActive( 'switchboard' ) && !empty( $storeComment ) && 
 			$headerHash['from_name'] = !empty( $userInfo['real_name'] ) ? $userInfo['real_name'] : $userInfo['login'];
 			$headerHash['sender'] = $userInfo['email'];
 		}
-		$gSwitchboardSystem->sendEmail( $storeComment->getTitle(), $storeComment->parseData(), $email, $headerHash );
+		$headerHash['x_headers']['X-BitBoards-Comment'] = $storeComment->mCommentId;
+		$messageId = $bitMailer->sendEmail( $storeComment->getTitle(), $storeComment->parseData(), $email, $headerHash );
+		$storeComment->storeMessageId( $messageId );
 	}
 }
 
