f090d0381a3c9bb53c717d71a1f9fc19a0915149
wjames5 <will@tekimaki.com>
2008-04-23 18:48:56 +0000

    gBoard changed to gComment

--- boards_comments_inc.php
+++ boards_comments_inc.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_boards/boards_comments_inc.php,v 1.3 2008/04/19 18:11:55 spiderr Exp $
- * $Id: boards_comments_inc.php,v 1.3 2008/04/19 18:11:55 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_boards/boards_comments_inc.php,v 1.4 2008/04/23 18:48:56 wjames5 Exp $
+ * $Id: boards_comments_inc.php,v 1.4 2008/04/23 18:48:56 wjames5 Exp $
  *
  * intermediate include file to provide centralized place to pre/post handle comments_inc include
  *
  * @author spider <spider@steelsun.com>
- * @version $Revision: 1.3 $ $Date: 2008/04/19 18:11:55 $ $Author: spiderr $
+ * @version $Revision: 1.4 $ $Date: 2008/04/23 18:48:56 $ $Author: wjames5 $
  * @package boards
  */
 
 
 require_once (LIBERTY_PKG_PATH.'comments_inc.php');
 
-if( !empty( $storeComment ) && $gBoard->getPreference('boards_mailing_list') ) {
+if( !empty( $storeComment ) && $gContent->getPreference('boards_mailing_list') ) {
 	if( empty( $storeComment->mErrors ) ) {
 		$storeComment->loadComment();
 		require_once( KERNEL_PKG_PATH.'BitMailer.php' );
 		$bitMailer = new BitMailer();
-		$email = $gBoard->getPreference('boards_mailing_list').'@'.$gBitSystem->getConfig( 'boards_email_host', $gBitSystem->getConfig( 'kernel_server_name' ) );
+		$email = $gContent->getPreference('boards_mailing_list').'@'.$gBitSystem->getConfig( 'boards_email_host', $gBitSystem->getConfig( 'kernel_server_name' ) );
 		$headerHash['mail_from'] = $gBitSystem->getConfig( 'boards_sync_user' ).'@'.$gBitSystem->getConfig( 'boards_sync_mail_server' );
 		if( $storeComment->getField( 'user_id' ) == ANONYMOUS_USER_ID ) {
 			$headerHash['from_name'] = $storeComment->getField( 'anon_name' );
